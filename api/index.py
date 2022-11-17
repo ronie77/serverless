5a331c0d9d41efe6db99f87a079a6ebf98c7b49b
Rohan Dilip P <73561095+ronie77@users.noreply.github.com>
2022-11-18 00:30:17 +0530

    update

--- api/index.py
+++ api/index.py
@@ -15,6 +15,5 @@ from flask import Flask, Response
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
+def hello(path):
     return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
