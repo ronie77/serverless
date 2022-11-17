a5e6e2a219653d06aaa8b3833f1aa959b109f2a0
Rohan Dilip P <73561095+ronie77@users.noreply.github.com>
2022-11-18 00:29:06 +0530

    update

--- api/index.py
+++ api/index.py
@@ -1,12 +1,20 @@
-from http.server import BaseHTTPRequestHandler
-from cowpy import cow
+# from http.server import BaseHTTPRequestHandler
+# from cowpy import cow
 
-class handler(BaseHTTPRequestHandler):
+# class handler(BaseHTTPRequestHandler):
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        message = cow.Cowacter().milk('Hello from Python from a Serverless Function!')
-        self.wfile.write(message.encode())
-        return
+#     def do_GET(self):
+#         self.send_response(200)
+#         self.send_header('Content-type','text/plain')
+#         self.end_headers()
+#         message = cow.Cowacter().milk('Hello from Python from a Serverless Function!')
+#         self.wfile.write(message.encode())
+#         return
+
+from flask import Flask, Response
+app = Flask(__name__)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
