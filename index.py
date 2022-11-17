0373924bf0986ec614796e66e9e232a9fa97b684
Rohan Dilip P <rohandilipp@gmail.com>
2022-11-17 23:57:25 +0530

    file changes

--- index.py
+++ index.py
@@ -1,7 +1,21 @@
-from flask import Flask, Response
+# from flask import Flask, Response
+# app = Flask(__name__)
+
+# @app.route('/', defaults={'path': ''})
+# @app.route('/<path:path>')
+# def catch_all(path):
+#     return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+
+
+from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    app.run()
