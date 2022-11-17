b484e72a2192b080718b35f199863a8c61985568
Rohan Dilip P <73561095+ronie77@users.noreply.github.com>
2022-11-18 00:38:56 +0530

    test

--- api/index.py
+++ api/index.py
@@ -17,3 +17,6 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def hello_world():
     return jsonify({'message' : 'Hello, World!'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
