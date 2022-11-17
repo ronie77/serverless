74ceeaf7b4d02ab01c48d8191638fb8d8829ba64
Rohan Dilip P <73561095+ronie77@users.noreply.github.com>
2022-11-18 00:10:44 +0530

    update

--- index.py
+++ index.py
@@ -1,21 +1,21 @@
-# from flask import Flask, Response
-# app = Flask(__name__)
-
-# @app.route('/', defaults={'path': ''})
-# @app.route('/<path:path>')
-# def catch_all(path):
-#     return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
-
-
-from flask import Flask
-
+from flask import Flask, Response
 app = Flask(__name__)
 
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+
+# from flask import Flask
+
+# app = Flask(__name__)
 
 
-if __name__ == "__main__":
-    app.run()
+# @app.route("/")
+# def hello():
+#     return "Hello World!"
+
+
+# if __name__ == "__main__":
+#     app.run()
